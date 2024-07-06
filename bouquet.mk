c368c002be4a7b96838bfeb1c8053e3624e7d8e5
Michael Bestas <mkbestas@lineageos.org>
2024-07-07 05:00:30 +0800

    bouquet-common: props: Use AOSP default Codec2/OMX ranks
    Change-Id: Idf55ec02c2c0f4d2fa73f949cffef0d820a54570

--- bouquet.mk
+++ bouquet.mk
@@ -337,9 +337,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
-
 # Mlipay
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.mlipay@1.1.vendor
